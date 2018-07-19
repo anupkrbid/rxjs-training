ea05c0fcfe6f10e2f9f73bdcd5e78f46be9c271b
anupkrbid <anupbid94@gmail.com>
2018-07-19 23:51:16 +0530

    Added Events and Properties

--- src/audio.service.ts
+++ src/audio.service.ts
@@ -1,13 +1,5 @@
-import {
-  BehaviorSubject,
-  fromEvent,
-  Observable,
-  Observer,
-  ReplaySubject,
-  Subject,
-  Subscription,
-} from "rxjs";
-import { withLatestFrom, takeUntil } from "rxjs/operators";
+import { fromEvent, Observable, Observer, Subject, Subscription } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 export class AudioService {
 
@@ -21,33 +13,64 @@ export class AudioService {
     const mediaEvents: { [key: string]: Observable<any> } = {};
     const mediaEventSubscriptions: { [key: string]: Subscription } = {};
     const mediaEventNames = [
-      "durationchange", //	The duration attribute has been updated.
-      "loadedmetadata", //	The metadata has been loaded.
-      "loadeddata", //	The first frame of the media has finished loading.
-      "canplay", //	The browser can play the media, but estimates that not enough data has been loaded to play the media up to its end without having to stop for further buffering of content.
-      "canplaythrough", //	The browser estimates it can play the media up to its end without stopping for content buffering.
-      "ended", //	Playback has stopped because the end of the media was reached.
-      "emptied", //	The media has become empty; for example, this event is sent if the media has already been loaded (or partially loaded), and the load() method is called to reload it.
-      "stalled", //	The user agent is trying to fetch media data, but data is unexpectedly not forthcoming.
-      "suspend", //	Media data loading has been suspended.
-      "play", //	Playback has begun.
-      "playing", //	Playback is ready to start after having been paused or delayed due to lack of data.
-      "pause", //	Playback has been paused.
-      "waiting", //	Playback has stopped because of a temporary lack of data.
-      "seeking", //	A seek operation began.
-      "seeked", //	A seek operation completed.
-      "ratechange", //	The playback rate has changed.
-      "timeupdate", //	The time indicated by the currentTime attribute has been updated.
-      "volumechange", //	The volume has changed.
-      "complete", //	The rendering of an OfflineAudioContext is terminated.
-      "audioprocess", //	The input buffer of a ScriptProcessorNode is ready to be processed.
-      "progress",
-      "error"
+      "audioprocess", // The input buffer of a ScriptProcessorNode is ready to be processed.
+      "complete", // The rendering of an OfflineAudioContext is terminated.
+
+      'abort', //	Fires when the loading of an audio/video is aborted
+      'canplay', //	Fires when the browser can start playing the audio/video
+      'canplaythrough', // Fires when the browser can play through the audio/video without stopping for buffering
+      'durationchange', // Fires when the duration of the audio/video is changed
+      'emptied', //	Fires when the current playlist is empty
+      'ended', //	Fires when the current playlist is ended
+      'error', //	Fires when an error occurred during the loading of an audio/video
+      'loadeddata', // Fires when the browser has loaded the current frame of the audio/video
+      'loadedmetadata', // Fires when the browser has loaded meta data for the audio/video
+      'loadstart', //	Fires when the browser starts looking for the audio/video
+      'pause', //	Fires when the audio/video has been paused
+      'play', // Fires when the audio/video has been started or is no longer paused
+      'playing', //	Fires when the audio/video is playing after having been paused or stopped for buffering
+      'progress', // Fires when the browser is downloading the audio/video
+      'ratechange', // Fires when the playing speed of the audio/video is changed
+      'seeked', // Fires when the user is finished moving/skipping to a new position in the audio/video
+      'seeking', // Fires when the user starts moving/skipping to a new position in the audio/video
+      'stalled', // Fires when the browser is trying to get media data, but data is not available
+      'suspend', // Fires when the browser is intentionally not getting media data
+      'timeupdate', // Fires when the current playback position has changed
+      'volumechange', // Fires when the volume has been changed
+      'waiting' // Fires when the video stops because it needs to buffer the next frame
     ];    
     const playerMetaData = {
-      src: '', // Hold the source of the currently playing track
       playing: false, // Holds state of player if audio is playing or not
-      currentTime: 0 // Gives the current playback time in seconds
+
+      audioTracks: '', // Returns an AudioTrackList object representing available audio tracks
+      autoplay: '', // Sets or returns whether the audio/video should start playing as soon as it is loaded
+      buffered: '', // Returns a TimeRanges object representing the buffered parts of the audio/video
+      controller: '', // Returns the MediaController object representing the current media controller of the audio/video
+      controls: '', // Sets or returns whether the audio/video should display controls (like play/pause etc.)
+      crossOrigin: '', // Sets or returns the CORS settings of the audio/video
+      currentSrc: '', // Returns the URL of the current audio/video
+      currentTime: 0, // Sets or returns the current playback position in the audio/video (in seconds)
+      defaultMuted: '', // Sets or returns whether the audio/video should be muted by default
+      defaultPlaybackRate: '', // Sets or returns the default speed of the audio/video playback
+      duration: '', // Returns the length of the current audio/video (in seconds)
+      ended: '', // Returns whether the playback of the audio/video has ended or not
+      error: '', // Returns a MediaError object representing the error state of the audio/video
+      loop: false, // Sets or returns whether the audio/video should start over again when finished
+      mediaGroup: '', // Sets or returns the group the audio/video belongs to (used to link multiple audio/video elements)
+      muted: false,	// Sets or returns whether the audio/video is muted or not
+      networkState: '', // Returns the current network state of the audio/video
+      paused: false, // Returns whether the audio/video is paused or not
+      playbackRate: '', // Sets or returns the speed of the audio/video playback
+      played: '', // Returns a TimeRanges object representing the played parts of the audio/video
+      preload: '', // Sets or returns whether the audio/video should be loaded when the page loads
+      readyState: '', // Returns the current ready state of the audio/video
+      seekable: '', // Returns a TimeRanges object representing the seekable parts of the audio/video
+      seeking: '', // Returns whether the user is currently seeking in the audio/video
+      src: '', // Sets or returns the current source of the audio/video element
+      startDate: '', // Returns a Date object representing the current time offset
+      textTracks: '', // Returns a TextTrackList object representing the available text tracks
+      videoTracks: '', // Returns a VideoTrackList object representing the available video tracks
+      volume: '' // Sets or returns the volume of the audio/video
     };
     const subjects = {
       play$: new Subject(), // custom event to start playing audio
@@ -123,3 +146,6 @@ sound.play(audioURL).subscribe((data: any) => {
 setTimeout(function () {
   sound.stop();
 }, 15000);
+
+// https://www.w3schools.com/tags/ref_eventattributes.asp
+// https://www.w3schools.com/tags/ref_av_dom.asp
