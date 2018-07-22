b976b11ef22493eac0ed3f29d79a83fbfc5f3122
anupkrbid <anupbid94@gmail.com>
2018-07-22 17:41:38 +0530

    modified audio service

--- src/audio.service.ts
+++ src/audio.service.ts
@@ -1,20 +1,18 @@
-import { fromEvent, Observable, Observer, Subject, Subscription } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import { fromEvent, Observable, Observer, Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 export class AudioService {
-
   private audio: HTMLAudioElement = new Audio();
   private stop$ = new Subject();
 
   constructor() {}
 
   playback(src: string) {
-
     const mediaEvents: { [key: string]: Observable<any> } = {};
     const mediaEventSubscriptions: { [key: string]: Subscription } = {};
     const mediaEventNames = [
-      "audioprocess", // The input buffer of a ScriptProcessorNode is ready to be processed.
-      "complete", // The rendering of an OfflineAudioContext is terminated.
+      'audioprocess', // The input buffer of a ScriptProcessorNode is ready to be processed.
+      'complete', // The rendering of an OfflineAudioContext is terminated.
 
       'abort', //	Fires when the loading of an audio/video is aborted
       'canplay', //	Fires when the browser can start playing the audio/video
@@ -38,29 +36,29 @@ export class AudioService {
       'timeupdate', // Fires when the current playback position has changed
       'volumechange', // Fires when the volume has been changed
       'waiting' // Fires when the video stops because it needs to buffer the next frame
-    ];    
+    ];
     const playerMetaData = {
       playing: false, // Holds state of player if audio is playing or not
 
       audioTracks: '', // Returns an AudioTrackList object representing available audio tracks
       autoplay: '', // Sets or returns whether the audio/video should start playing as soon as it is loaded
-      buffered: '', // Returns a TimeRanges object representing the buffered parts of the audio/video
+      buffered: 0, // Returns a TimeRanges object representing the buffered parts of the audio/video
       controller: '', // Returns the MediaController object representing the current media controller of the audio/video
       controls: '', // Sets or returns whether the audio/video should display controls (like play/pause etc.)
       crossOrigin: '', // Sets or returns the CORS settings of the audio/video
       currentSrc: '', // Returns the URL of the current audio/video
       currentTime: 0, // Sets or returns the current playback position in the audio/video (in seconds)
-      defaultMuted: '', // Sets or returns whether the audio/video should be muted by default
-      defaultPlaybackRate: '', // Sets or returns the default speed of the audio/video playback
-      duration: '', // Returns the length of the current audio/video (in seconds)
-      ended: '', // Returns whether the playback of the audio/video has ended or not
+      defaultMuted: false, // Sets or returns whether the audio/video should be muted by default
+      defaultPlaybackRate: 1, // Sets or returns the default speed of the audio/video playback
+      duration: 0, // Returns the length of the current audio/video (in seconds)
+      ended: false, // Returns whether the playback of the audio/video has ended or not
       error: '', // Returns a MediaError object representing the error state of the audio/video
       loop: false, // Sets or returns whether the audio/video should start over again when finished
       mediaGroup: '', // Sets or returns the group the audio/video belongs to (used to link multiple audio/video elements)
-      muted: false,	// Sets or returns whether the audio/video is muted or not
+      muted: false, // Sets or returns whether the audio/video is muted or not
       networkState: '', // Returns the current network state of the audio/video
       paused: false, // Returns whether the audio/video is paused or not
-      playbackRate: '', // Sets or returns the speed of the audio/video playback
+      playbackRate: 0, // Sets or returns the speed of the audio/video playback
       played: '', // Returns a TimeRanges object representing the played parts of the audio/video
       preload: '', // Sets or returns whether the audio/video should be loaded when the page loads
       readyState: '', // Returns the current ready state of the audio/video
@@ -70,7 +68,7 @@ export class AudioService {
       startDate: '', // Returns a Date object representing the current time offset
       textTracks: '', // Returns a TextTrackList object representing the available text tracks
       videoTracks: '', // Returns a VideoTrackList object representing the available video tracks
-      volume: '' // Sets or returns the volume of the audio/video
+      volume: 1 // Sets or returns the volume of the audio/video
     };
     const subjects = {
       play$: new Subject(), // custom event to start playing audio
@@ -83,7 +81,7 @@ export class AudioService {
       mediaEventNames.forEach(event => {
         mediaEvents[`${event}$`] = fromEvent(this.audio, event);
       });
-    }
+    };
 
     const removeEventListeners = () => {
       for (const subscription in mediaEventSubscriptions) {
@@ -91,28 +89,211 @@ export class AudioService {
           mediaEventSubscriptions[subscription].unsubscribe();
         }
       }
-    }
+    };
 
     return Observable.create((observer: Observer<any>) => {
-
       addEventListeners();
 
-      mediaEventSubscriptions.timeupdateSubscription = 
-        mediaEvents.timeupdate$.subscribe(() => {
+      // 'abort' //	Fires when the loading of an audio/video is aborted
+      mediaEventSubscriptions.abortSubscription = mediaEvents.abort$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('abort event fired!');
+        }
+      );
+
+      // 'canplay' // Fires when the browser can start playing the audio/video
+      mediaEventSubscriptions.canplaySubscription = mediaEvents.canplay$.subscribe(
+        () => {
+          this.audio.play();
+          observer.next(playerMetaData);
+          logEvents('canplay event fired!');
+        }
+      );
+
+      // 'canplaythrough' // Fires when the browser can play through the audio/video without stopping for buffering
+      mediaEventSubscriptions.canplaythroughSubscription = mediaEvents.canplaythrough$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('canplaythrough event fired!');
+        }
+      );
+
+      // 'durationchange' // Fires when the duration of the audio/video is changed
+      mediaEventSubscriptions.durationchangeSubscription = mediaEvents.durationchange$.subscribe(
+        () => {
+          playerMetaData.duration = this.audio.duration;
+          observer.next(playerMetaData);
+          logEvents('durationchange event fired!');
+        }
+      );
+
+      // 'emptied' //	Fires when the current playlist is empty
+      mediaEventSubscriptions.emptiedSubscription = mediaEvents.emptied$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('emptied event fired!');
+        }
+      );
+
+      // 'ended' //	Fires when the current playlist is ended
+      mediaEventSubscriptions.endedSubscription = mediaEvents.ended$.subscribe(
+        () => {
+          alert('ended');
+          observer.next(playerMetaData);
+          logEvents('ended event fired!');
+        }
+      );
+
+      // 'error' //	Fires when an error occurred during the loading of an audio/video
+      mediaEventSubscriptions.errorSubscription = mediaEvents.error$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('error event fired!');
+        }
+      );
+
+      // 'loadeddata' // Fires when the browser has loaded the current frame of the audio/video
+      mediaEventSubscriptions.loadeddataSubscription = mediaEvents.loadeddata$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('loadeddata event fired!');
+        }
+      );
+
+      // 'loadedmetadata' // Fires when the browser has loaded meta data for the audio/video
+      mediaEventSubscriptions.loadedmetadataSubscription = mediaEvents.loadedmetadata$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('loadedmetadata event fired!');
+        }
+      );
+
+      // 'loadstart' //	Fires when the browser starts looking for the audio/video
+      mediaEventSubscriptions.loadstartSubscription = mediaEvents.loadstart$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('loadstart event fired!');
+        }
+      );
+
+      // 'pause' //	Fires when the audio/video has been paused
+      mediaEventSubscriptions.pauseSubscription = mediaEvents.pause$.subscribe(
+        () => {
+          playerMetaData.paused = this.audio.paused;
+          playerMetaData.playing = !this.audio.paused;
+          observer.next(playerMetaData);
+          logEvents('pause event fired!');
+        }
+      );
+
+      // 'play' // Fires when the audio/video has been started or is no longer paused
+      mediaEventSubscriptions.playSubscription = mediaEvents.play$.subscribe(
+        () => {
+          playerMetaData.paused = false;
+          playerMetaData.playing = true;
+          observer.next(playerMetaData);
+          logEvents('play event fired!');
+        }
+      );
+
+      // 'playing' //	Fires when the audio/video is playing after having been paused or stopped for buffering
+      mediaEventSubscriptions.playingSubscription = mediaEvents.playing$.subscribe(
+        () => {
+          playerMetaData.paused = false;
+          playerMetaData.playing = true;
+          observer.next(playerMetaData);
+          logEvents('playing event fired!');
+        }
+      );
+
+      // 'progress' // Fires when the browser is downloading the audio/video
+      mediaEventSubscriptions.progressSubscription = mediaEvents.progress$.subscribe(
+        () => {
+          // playerMetaData.buffered = this.audio.buffered.end(0);
+          // playerMetaData.seekable = this.audio.seekable;
+          observer.next(playerMetaData);
+          logEvents('progress event fired!');
+        }
+      );
+
+      // 'ratechange' // Fires when the playing speed of the audio/video is changed
+      mediaEventSubscriptions.ratechangeSubscription = mediaEvents.ratechange$.subscribe(
+        () => {
+          playerMetaData.playbackRate = this.audio.playbackRate;
+          observer.next(playerMetaData);
+          logEvents('ratechange event fired!');
+        }
+      );
+
+      // 'seeked' // Fires when the user is finished moving/skipping to a new position in the audio/video
+      mediaEventSubscriptions.seekedSubscription = mediaEvents.seeked$.subscribe(
+        () => {
           playerMetaData.currentTime = this.audio.currentTime;
           observer.next(playerMetaData);
-        });
+          logEvents('seeked event fired!');
+        }
+      );
 
-      mediaEventSubscriptions.canplaySubscription = 
-        mediaEvents.canplay$.subscribe(() => this.audio.play());
-      
+      // 'seeking' // Fires when the user starts moving/skipping to a new position in the audio/video
+      mediaEventSubscriptions.seekingSubscription = mediaEvents.seeking$.subscribe(
+        () => {
+          playerMetaData.currentTime = this.audio.currentTime;
+          observer.next(playerMetaData);
+          logEvents('seeking event fired!');
+        }
+      );
+
+      // 'stalled' // Fires when the browser is trying to get media data, but data is not available
+      mediaEventSubscriptions.stalledSubscription = mediaEvents.stalled$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('stalled event fired!');
+        }
+      );
+
+      // 'suspend' // Fires when the browser is intentionally not getting media data
+      mediaEventSubscriptions.suspendSubscription = mediaEvents.suspend$.subscribe(
+        () => {
+          observer.next(playerMetaData);
+          logEvents('suspend event fired!');
+        }
+      );
+
+      // 'timeupdate' // Fires when the current playback position has changed
+      mediaEventSubscriptions.timeupdateSubscription = mediaEvents.timeupdate$.subscribe(
+        () => {
+          playerMetaData.currentTime = this.audio.currentTime;
+          playerMetaData.buffered = this.audio.buffered.end(0);
+          observer.next(playerMetaData);
+          logEvents('timeupdate event fired!');
+        }
+      );
+
+      // 'volumechange' // Fires when the volume has been changed
+      mediaEventSubscriptions.volumechangeSubscription = mediaEvents.volumechange$.subscribe(
+        () => {
+          playerMetaData.volume = this.audio.volume;
+          observer.next(playerMetaData);
+          logEvents('volumechange event fired!');
+        }
+      );
+
+      // 'waiting' // Fires when the video stops because it needs to buffer the next frame
+      mediaEventSubscriptions.waitingSubscription = mediaEvents.waiting$.subscribe(
+        () => {
+          logEvents('waiting event fired!');
+          observer.next(playerMetaData);
+        }
+      );
+
       this.audio.src = src;
       playerMetaData.src = src;
 
       // observer.next(sharableSubjects);
 
       return () => {
-        log('removing!');
+        logEvents('unsubscribing!');
         this.audio.pause();
         this.audio.src = '';
         removeEventListeners();
@@ -129,21 +310,29 @@ export class AudioService {
   }
 }
 
-function log(val: any) {
-  var node = document.createElement("li");
+var eventDiv = document.querySelector('#events');
+function logEvents(val: any) {
+  var node = document.createElement('li');
   var textnode = document.createTextNode(val);
   node.appendChild(textnode);
-  document.getElementById("output").appendChild(node);
-  window.scrollTo(0, document.body.scrollHeight);
+  eventDiv.appendChild(node);
+  // window.scrollTo(0, document.body.scrollHeight);
+  eventDiv.scrollTop = eventDiv.scrollHeight;
+}
+
+var observableDivCode = document.querySelector('#observable code');
+function logObs(val: any) {
+  observableDivCode.innerHTML = val;
 }
 
 const sound = new AudioService();
-const audioURL = 'http://www.hipstrumentals.com/wp-content/uploads/2014/10/Eve-Ft.-Gwen-Stefani-Let-Me-Blow-Ya-Mind-Instrumental-Prod.-By-Swizz-Beatz.mp3';
+const audioURL =
+  'http://www.hipstrumentals.com/wp-content/uploads/2014/10/Eve-Ft.-Gwen-Stefani-Let-Me-Blow-Ya-Mind-Instrumental-Prod.-By-Swizz-Beatz.mp3';
 sound.play(audioURL).subscribe((data: any) => {
-  log(data.currentTime);
+  logObs(JSON.stringify(data, null, 4));
 });
 
-setTimeout(function () {
+setTimeout(function() {
   sound.stop();
 }, 15000);
 
